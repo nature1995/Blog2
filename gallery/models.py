f77538ccf9bf99a10549ec47ce5cfe4a21823204
nature <gzr2008@vip.qq.com>
2018-08-03 17:07:29 +0800

    Version1.0
    导入Version1.0

---
+++ gallery/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Gallery(models.Model):
+	description = models.CharField(default='在这里写作品的简洁', max_length=100)
+	title = models.CharField(default='作品标题', max_length=50)
+	image = models.ImageField(default='default.png',upload_to='images/')
+
+	def __str__(self):
+		return  self.title
+
+
+    	
+
